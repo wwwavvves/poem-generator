97cc1526902e3d09f72e679472175d8580082c01
Diana <wwwavvves@gmail.com>
2024-06-25 12:23:51 +0100

    Installed typewriter

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
 let poemFormElement = document.querySelector("#poem-generator-form");
 let inputBtn = document.querySelector("#topic-input");
 let poemElement = document.querySelector("#poem");
+// let prompt = inputBtn.value;
 
 poemFormElement.addEventListener("submit", writePoem);
 
 function writePoem(event) {
   event.preventDefault();
-  alert(inputBtn.value);
+  new Typewriter(poemElement, {
+    strings: "Threads of Memory", // answer
+    autoStart: true,
+    cursor: "",
+    delay: 80,
+  });
 }
