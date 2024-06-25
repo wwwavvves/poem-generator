2dc0a271b898b95e651062bc2866f6e740bc03d8
Diana <wwwavvves@gmail.com>
2024-06-25 16:22:33 +0100

    Improved UX

--- src/script.js
+++ src/script.js
@@ -1,23 +1,20 @@
-let poemFormElement = document.querySelector("#poem-generator-form");
-let inputBtn = document.querySelector("#topic-input");
-let poemElement = document.querySelector("#poem");
-
-poemFormElement.addEventListener("submit", generatePoem);
-
 function generatePoem(event) {
   event.preventDefault();
+  let inputBtn = document.querySelector("#topic-input");
+  poemElement.classList.remove("invisible");
 
   let apiKey = "68a066fb34dtb3fc9d4875c8d3bo09b6";
   let context =
-    "You are very knowledgeable about the poetry of Li-Young Lee, Frank O'Hara, Yoko Ono and Louise Glück and you are brilliant at writing short poems inspired by these writers. Please sign the poem at the end with 'SheCodes AI' between a <strong> element in the HTML";
+    "You are very knowledgeable about the poetry of Li-Young Lee, Frank O'Hara, Yoko Ono and Louise Glück and you are brilliant at writing short poems inspired by these writers. Please sign the poem at the end with 'SheCodes AI' ALWAYS between a <strong> element in the HTML and NEVER in italic";
   let prompt = `Generate an original poem with two stanzas in the style of Li-Young Lee, Frank O'Hara, Yoko Ono and Louise Glück mixed together about ${inputBtn.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  poemElement.innerHTML = "Generating poem...";
+  poemElement.innerHTML = `<div class="flickering">Generating a poem...</div>`;
   axios.get(apiUrl).then(doSomething);
 }
 
 function doSomething(response) {
+  poemElement.classList.remove("flickering");
   let answer = response.data.answer.replace(/\n/g, "<br>");
   console.log(answer);
 
@@ -28,3 +25,7 @@ function doSomething(response) {
     delay: 50,
   });
 }
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
+let poemElement = document.querySelector("#poem");
